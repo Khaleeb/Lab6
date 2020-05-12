2538ce04861c9abb18017fc71eda99e87ae66cb4
Caleb Davis <cdavis1530@gmail.com>
2020-05-11 21:51:59 -0400

    Added PieceHeadDean

---
+++ java181/src/main/java/cisc181/lab_6/PieceHeadDean.java
@@ -0,0 +1,28 @@
+package cisc181.lab_6;
+
+// Piece that implements recruiter, L-shape movement, one per team, win condition+
+public class PieceHeadDean extends Piece implements Recruiter {
+    public PieceHeadDean(String symbol, String color){
+        super(symbol, color);
+    }
+
+    public PieceHeadDean(String symbol){
+        this(symbol, "");
+    }
+
+    // Speak Method
+    public void speak() {
+        System.out.println("Head Dean Reporting in.");
+    }
+
+    // L-Shape Path
+    public boolean validPath(int rowI, int colI, int row, int col) {
+        return ((Math.abs(row - rowI) == 2 && Math.abs(col - colI) == 1) || (Math.abs(row - rowI) == 1 && Math.abs(col - colI) == 2));
+    }
+
+    // Recruiter method
+    public void recruit(int firstSpaceRow, int firstSpaceCol, int secondSpaceRow, int secondSpaceCol){
+        speak();
+    }
+
+}
